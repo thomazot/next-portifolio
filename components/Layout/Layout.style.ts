bb4a2db961691cc9c7d32a54900bb58d575c3ae7
Thomaz T. O. Toyama <thomazot@gmail.com>
2021-03-21 01:30:53 -0300

    img max-width

--- components/Layout/Layout.style.ts
+++ components/Layout/Layout.style.ts
@@ -7,7 +7,9 @@ export const Container = styled.div`
     margin: 0 auto;
     font-size: 14px;
     line-height: 1.4;
-
+    img {
+      max-width: 100%;
+    }
     ul {
       padding-left: ${({ theme }) => `${theme.gap}px`};
     }
