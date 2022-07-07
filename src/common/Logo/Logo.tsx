8ad81b427b90a327298c9b8448cf363b965e973d
Thomaz Takashi Oda Toyama <thomaz@gmail.com>
2022-07-07 18:48:03 -0300

    refactore: Layout update

--- src/common/Logo/Logo.tsx
+++ src/common/Logo/Logo.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import LogoIcon from 'styles/imgs/favicon.svg'
 
-const Logo: React.FC = (props) => (
+const Logo: React.FC<{ width?: string; height?: string; title?: string }> = (
+  props
+) => (
   <Link href="/">
     <a>
       <LogoIcon {...props} />
