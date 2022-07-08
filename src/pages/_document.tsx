23c5b9552f4cc89e2e4096869bc6debddf4b80d9
Thomaz Takashi Oda Toyama <thomaz@gmail.com>
2022-07-08 01:04:48 -0300

    feat: add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -58,6 +58,21 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-DJG5PC48P0"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DJG5PC48P0');
+          `
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
